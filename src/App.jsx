0f1c56cb1a6578cb4737094163d892cbee6d0955
Tomasz Master <tsierzputowski@op.pl>
2021-07-22 10:55:50 +0200

    feet implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import StoreProvider from "./store/StoreProvider";
 import "./App.scss";
 
-const App = () => <header>hello world</header>;
+const App = () => (
+  <StoreProvider>
+    <header>hello world!!</header>
+  </StoreProvider>
+);
+
 export default App;
