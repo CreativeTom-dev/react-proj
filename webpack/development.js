4982d2e0d7c00e642fade668dbec4e42bb035ce6
Tomasz Master <tsierzputowski@op.pl>
2021-07-25 21:09:52 +0200

    git

--- webpack/development.js
+++ webpack/development.js
@@ -1,44 +1,45 @@
 module.exports = {
-    devServer: {
-        contentBase: './public',
-        port: 3000,
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [{
-                test: /\.module\.s(a|c)ss$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                localIdentName: '[local]',
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        }
-                    }
-                ]
+  devServer: {
+    contentBase: "./public",
+    port: 3000,
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.module\.s(a|c)ss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[local]",
+              },
             },
-            {
-                test: /\.(s(a|c)ss|css)$/,
-                exclude: /\.module.(s(a|c)ss)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-}
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(s(a|c)ss|css)$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
